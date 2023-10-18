0329677567adf469570cb903b226d3f94e4c3c62
123Somrat <jafaruddin875@gmail.com>
2023-10-18 13:58:47 +0200

    create collection for clothes

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.use(express.json());
 
 // create route 
 
-app.get("/addidas",(req,res)=>{
-       res.json(addidas)  
-})
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -32,6 +29,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("clothesDb");
+    const haiku = database.collection("clothes");
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
